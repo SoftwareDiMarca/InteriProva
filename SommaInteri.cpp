3c4b7a4a11f53978d4e0217393b6a7bb04da7936
SoftwareDiMarca <maurizio.marcaccini@studio.unibo.it>
2018-03-24 08:50:55 +0100

    Versione finale

--- SommaInteri.cpp
+++ SommaInteri.cpp
@@ -12,14 +12,14 @@ int main(){
 
 	//programma puntatori a intero
 
-	int intero1, intero2, intero3;
+	int intero1, intero2, intero4;
 	cout <<"inserisci il primo intero\n";
 	cin >> intero1;
 	cout << "Inserisci il secondo intero\n";
 	cin >>intero2;
-	cout << "Inserisci il terzo intero\n";
-	cin >> intero3;
-	cout << "Somma di " << intero1 << " con " << intero2 << " e con " << intero3 << " = " << intero1+intero2+intero3 << endl;
+	cout << "Inserisci il quarto intero\n";
+	cin >> intero4;
+	cout << "Somma di " << intero1 << " con " << intero2 << " e con " << intero4 << " = " << intero1+intero2+intero4 << endl;
 
 	int temp;
 	if (intero1 > intero2){
@@ -27,18 +27,18 @@ int main(){
 		intero2 = intero1;
 		intero1 = temp;
 	}
-	if (intero3 > intero1 && intero3 < intero2){
+	if (intero4 > intero1 && intero4 < intero2){
 		temp = intero2;
-		intero2 = intero3;
-		intero3 = temp;
+		intero2 = intero4;
+		intero4 = temp;
 	}
-	if (intero3 < intero1){
+	if (intero4 < intero1){
 		temp = intero1;
-		intero1 = intero3;
-		intero3 = intero2;
+		intero1 = intero4;
+		intero4 = intero2;
 		intero2 = temp;
 	}
-		cout << "Sequenza ordinata dei numeri immessi: " << intero1 << " - " << intero2 << " - " << intero3;
+		cout << "Sequenza ordinata dei numeri immessi: " << intero1 << " - " << intero2 << " - " << intero4;
 	return 0;
 }
 
